d6ac61e7ad6d8f146e36afabc66fe876804cce38
merdansahin <merdansahin68@gmail.com>
2026-01-18 00:47:32 +0300

    controllers data

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -12,11 +12,6 @@ const contactSchema = new Schema(
       required: true,
       default: 'personal',
     },
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
   },
   { timestamps: true }
 );
