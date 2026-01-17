94e7569d53fc133c46acb1e0b270b1ccb9290733
merdansahin <merdansahin68@gmail.com>
2026-01-18 00:41:44 +0300

    controllers data

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,7 @@ export const getContactByIdController = async (req, res) => {
   res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
-    data: result,
+    data: contact,
   });
 };
 export const createContactController = async (req, res) => {
